9792ab371472caade180a046a51a9e65c4b66685
daffa.doj <daffa.doj@gmail.com>
2025-11-20 22:25:37 +0700

    feat: change main menu to HUDs

--- main_menu.gd
+++ main_menu.gd
@@ -1,20 +1,16 @@
 extends Control
 
+@onready var pausePanel: Panel = $PausePanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pausePanel.visible = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_start_button_up() -> void:
-	get_tree().change_scene_to_file("res://temp_game_scene.tscn")
-	pass # Replace with function body.
-
 
 func _on_option_button_up() -> void:
 	print("Settings clicked")
@@ -23,4 +19,20 @@ func _on_option_button_up() -> void:
 
 func _on_quit_button_up() -> void:
 	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_back_button_up() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_pause_button_up() -> void:
+	pausePanel.visible = true
+	get_tree().paused = false
+	pass # Replace with function body.
+
+
+func _on_resume_button_up() -> void:
+	pausePanel.visible = false
+	get_tree().paused = true
 	pass # Replace with function body.

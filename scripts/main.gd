18bc3aa21ed657fb0f98531fdb1ac0917e46a1e4
daffa.doj <daffa.doj@gmail.com>
2025-11-21 02:49:31 +0700

    fix: use viewport size instead of constant number

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,15 +5,18 @@ const camera_start := Vector2i(576, 321)
 
 const speed :float = 500.0
 
+var viewportX
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	new_game()
 
 func new_game():
+	viewportX = get_viewport().size.x
 	$Outlaw.position = outlaw_start
 	$Camera2D.position = camera_start
-	$Ground.position = Vector2i(576,620)
-	$Ground2.position = Vector2i(1728,620)
+	$Ground.position = Vector2i(viewportX/2,620)
+	$Ground2.position = Vector2i((viewportX/2)*3,620)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,7 +24,7 @@ func _process(delta: float) -> void:
 	$Ground2.position.x += -speed * delta
 	$Outlaw.actions()
 	
-	if $Ground.position.x <= -576:
-		$Ground.position.x = 1728
-	elif $Ground2.position.x <= -576:
-		$Ground2.position = 1728
+	if $Ground.position.x <= -viewportX/2:
+		$Ground.position.x = (viewportX/2)*3
+	elif $Ground2.position.x <= -viewportX/2:
+		$Ground2.position.x = (viewportX/2)*3

62da08b7ac3ce1fb41a3bb8e4fac6d6ddb60f28c
daffa.doj <daffa.doj@gmail.com>
2025-11-22 21:45:15 +0700

    feat: add other form of dodge attack

--- scripts/bosses/boss_1.gd
+++ scripts/bosses/boss_1.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-var skill_1 = preload("res://scenes/bosses/boss_1_skills/dodge.tscn")
+var state_time = 0
+var dodge_1 = preload("res://scenes/bosses/boss_1_skills/dodge.tscn")
+var dodge_2 = preload("res://scenes/bosses/boss_1_skills/dodge_1.tscn")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,29 +14,46 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func dodge_skill() -> void:
-	var skill_instance : Node2D = skill_1.instantiate()
-	skill_instance.global_position = Vector2(0, (get_viewport().size.y/2))
-	$"../Camera2D".add_child(skill_instance)
+func phase_1() -> void:
+	var rando = randi_range(1, 3)
+	if rando == 1:
+		var skill_instance : Node2D = dodge_1.instantiate()
+		skill_instance.global_position = Vector2(0, (get_viewport().size.y/2))
+		$"../Camera2D".add_child(skill_instance)
+	if rando == 2:
+		var skill_instance : Node2D = dodge_2.instantiate()
+		skill_instance.global_position = Vector2(-(get_viewport().size.x/2), (get_viewport().size.y/3))
+		$"../Camera2D".add_child(skill_instance)
+	if rando == 3:
+		var skill_instance : Node2D = dodge_2.instantiate()
+		skill_instance.global_position = Vector2(-(get_viewport().size.x/2), -(get_viewport().size.y/4))
+		$"../Camera2D".add_child(skill_instance)
 	pass
 	
-func ranged_skill() -> void:
+func phase_1_to_2() -> void:
 	
 	pass
 	
-func melee_skill() -> void:
+func phase_2() -> void:
+	
+	pass
+	
+func phase_3() -> void:
 	
 	pass
 
 func _on_timer_timeout() -> void:
-	var rndm = randi_range(0,2)
-	if rndm == 0:
-		dodge_skill()
+	state_time+=1 
+	if state_time < 10:
+		phase_1()
 		pass
-	if rndm == 1:
-		ranged_skill()
+	elif state_time == 10: 
+		phase_1_to_2()
+	elif state_time < 20:
+		phase_2()
 		pass
-	if rndm == 2:
-		melee_skill()
+	elif state_time < 30:
+		phase_3()
 		pass
+	
 	pass # Replace with function body.

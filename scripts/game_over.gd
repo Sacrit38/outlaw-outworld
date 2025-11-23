e56595ff30dd7614917b12e845ac7ed2a447ecc2
daffa.doj <daffa.doj@gmail.com>
2025-11-23 21:33:08 +0700

    fix: fix restart after losing

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -17,8 +17,17 @@ func show_game_over():
 	$"../AnimationPlayer".play_backwards("blur")
 
 func _on_restart_pressed() -> void:
+	health.set_health(5)
+	health.set_max_health(5)
 	get_tree().paused = false
+	$"../AnimationPlayer".play_backwards("blur")
+	hide()
+	Global.check_high_score()
 	get_tree().reload_current_scene()
+	var player = get_node("../../Outlaw")
+	if player.state != player.STATE_RUN:
+		player.state = player.STATE_RUN
+	Global.game_running = false
 
 func _on_quit_pressed() -> void:
 	get_tree().quit() 
